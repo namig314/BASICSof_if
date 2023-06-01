8cf8d6aeabcca97b25d07832ae9e7e0fc3930093
namig314 <133330281+namig314@users.noreply.github.com>
2023-06-01 17:33:43 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+// let value = 20;
+
+// console.log(value === 10 );
+// console.log(value <=11 );
+// console.log(value != '20');
+// console.log(value == "10" );
+// console.log(value <=19 );
+
+
+
+
+
+
+
+
+//  EXERCISE 1            i guess it is right
+
+// let name = 'Tony';
+// let surname = 'Marcos';
+// fullName =name + " " + surname;
+// console.log(fullName[0]);
+
+//  EXERCISE 2         i guess it is right
+
+// let query = prompt('please write your fullname here');
+// let name = 'Tony';
+// let surname ='Marcos';
+// if(query == name + ' ' + surname){
+//   console.log(`"${surname} ${name}"`);
+// }
+
+// EXERCISE 3     i gues it is right
+
+// let query = prompt('please enter yor name?');
+// if(query=='Marry'){
+//   console.log(`"Hi, ${query}!"`)
+// }
+
+
+// EXERCISE 4        i guess it is right
+
+// let query = +prompt('please enter first number');
+// let secondQuery = +prompt('please enter second number');
+// console.log(query + secondQuery);
+// console.log(query - secondQuery);
+// console.log(query * secondQuery);
+// console.log(query / secondQuery);
+
+
+
+// EXERCISE 5     unfortunately unfinished
+
+// let firstNum = 12345;
+// let secondNum =98765;
+// let eventual =firstNum * secondNum;
+// console.log(eventual);
+
+
+
+// EXERCISE 6           unfortunately  i couldnt
+
+
+
+
+// EXERCISE 7         i guess it is right
+
+// let query = +prompt('please add the number of 530.90 which is price')
+
+// if (query==530.90) {
+// console.log('its price is "530 manat 90 cent."');
+// }
+
+
+
+// // EXERCISE 8        i guess it is right
+
+// let query = prompt('please introduce yourself like this: surname,name and father name ');
+// let surname = 'Mccarty';
+// let name = 'Jack';
+// let fatherName = 'William';
+// if(query==surname + ' ' + name + ' ' + fatherName ){
+//   console.log(surname[0]);
+//   console.log(name[0]);
+//   console.log(fatherName[0]);
+// }
+
+
+
+
+
+
+
+
+//  !!!!!!!!!!!!!!!!!!   OTHERS  !!!!!!!!!!!!!!!!!!!!
+
+//  let firstQuery = prompt('please attach your username');
+// let secondQuery =prompt('please attach your password');
+// let username ='Tony';
+// let password = 12345;
+
+
+// if ((username == 'Tony') && (password == 12345)){
+// alert('you are welcome');
+// }
